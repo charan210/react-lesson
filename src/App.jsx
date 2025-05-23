4f110baa2d3fc37da93db6331e347fe91380c9aa
charan210 <reddycharan2580@gmail.com>
2025-05-23 20:24:34 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import App1 from "./components/App1";
+import App2 from "./components/App2";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 function App() {
-
   return (
-    <>
-      <h1>React Lessons</h1>
-    </>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route index element={<App1 />}></Route>
+        <Route path="/" element={<App1 />}></Route>
+        <Route path="/app2" element={<App2 />}></Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
-
-export default App
+export default App;
